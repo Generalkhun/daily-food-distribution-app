da031e38df68ac08f7e022c7f863ed5ac9ca8030
Generalkhun <khun2551@hotmail.com>
2021-09-08 15:03:04 +0700

    Add confirmation modal

--- client-admin/src/components/VillagerConsoleBox.tsx
+++ client-admin/src/components/VillagerConsoleBox.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -8,7 +8,8 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { VillagerHomeData } from "../type";
-import { Divider } from "@material-ui/core";
+import { Divider, FormControlLabel, Switch } from "@material-ui/core";
+import ModalConfirmStatusChange from "./ModalConfirmStatusChange";
 
 const useStyles = makeStyles({
   root: {
@@ -25,58 +26,90 @@ interface Props {
 export default function VillagerConsoleBox(props: Props) {
   const classes = useStyles();
   const { selectedVillagerInfo } = props;
-  const lat = (selectedVillagerInfo.homeLocation||[0,0])[0]
-  const lng = (selectedVillagerInfo.homeLocation||[0,0])[1]
+  const lat = (selectedVillagerInfo.homeLocation || [0, 0])[0];
+  const lng = (selectedVillagerInfo.homeLocation || [0, 0])[1];
+
+  const [isGetFood, setIsGetFood] = useState<boolean>(
+    selectedVillagerInfo.isFoodRecieved || true
+  );
+
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+
+  const toggleGetFoodStatus = () => {
+    setIsGetFood((prevStatus) => !prevStatus);
+    handleOpenModal();
+    console.log("ส่งแล้วว");
+  };
+  const handleOpenModal = () => {
+    setIsOpenModal(true);
+  };
+  const handleCloseModal = () => {
+    setIsOpenModal(false);
+  };
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={selectedVillagerInfo.homeRepresentativesImg}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {selectedVillagerInfo.homeRepresentativesName}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {`เบอร์ติดต่อ: ${selectedVillagerInfo.homeRepresentativesContactNum}`}
-          </Typography>
-          <Divider />
-          <Typography variant="body2" color="textSecondary" component="p">
-            {"จำนวนสมาชิกในบ้าน:" +
-              " " +
-              selectedVillagerInfo.numberOfFamilyMember}
-          </Typography>
-          <Divider />
-          <Typography variant="body2" color="textSecondary" component="p">
-            {`สถานะ: ${
-              selectedVillagerInfo.isFoodRecieved
-                ? "ได้รับข้าวแล้ว"
-                : "ยังไม่ได้รับข้าว"
-            }`}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href={`https://www.google.com/maps/search/?api=1&query=${lat},${lng}`}
-          >
-            ดูเส้นทางจาก goole map
-          </a>
-        </Button>
-        <Button size="small" color="primary">
-          <img
-            width="40"
-            height="20"
-            src="https://upload.wikimedia.org/wikipedia/commons/4/41/LINE_logo.svg"
+    <>
+      <ModalConfirmStatusChange
+        isOpenModal={isOpenModal}
+        handleCloseModal={handleCloseModal}
+      />
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={selectedVillagerInfo.homeRepresentativesImg}
+            title="Contemplative Reptile"
           />
-          {"ติดต่อไลน์ >"}
-        </Button>
-      </CardActions>
-    </Card>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {selectedVillagerInfo.homeRepresentativesName}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {`เบอร์ติดต่อ: ${selectedVillagerInfo.homeRepresentativesContactNum}`}
+            </Typography>
+            <Divider />
+            <Typography variant="body2" color="textSecondary" component="p">
+              {"จำนวนสมาชิกในบ้าน:" +
+                " " +
+                selectedVillagerInfo.numberOfFamilyMember}
+            </Typography>
+            <Divider />
+            <Typography variant="body2" color="textSecondary" component="p">
+              {`สถานะ: ${
+                selectedVillagerInfo.isFoodRecieved
+                  ? "ได้รับข้าวแล้ว"
+                  : "ยังไม่ได้รับข้าว"
+              }`}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={`https://www.google.com/maps/search/?api=1&query=${lat},${lng}`}
+            >
+              ดูเส้นทางจาก goole map
+            </a>
+          </Button>
+          <Button size="small" color="primary">
+            <img
+              width="40"
+              height="20"
+              src="https://upload.wikimedia.org/wikipedia/commons/4/41/LINE_logo.svg"
+            />
+            {"ติดต่อไลน์ >"}
+          </Button>
+        </CardActions>
+        <CardActions>
+          <FormControlLabel
+            control={
+              <Switch checked={isGetFood} onChange={toggleGetFoodStatus} />
+            }
+            label="ส่งสำเร็จแล้ว"
+          />
+        </CardActions>
+      </Card>
+    </>
   );
 }
